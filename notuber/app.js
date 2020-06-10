82ff5a53487977c0383126c758b04608f3e72e38
Camelia D. Brumar <cbrumar@umiacs.umd.edu>
2020-06-10 17:37:18 -0400

    added the markers to the map

---
+++ notuber/app.js
@@ -0,0 +1,55 @@
+(function(exports) {
+    "use strict";
+
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById("map"), {
+            center: {
+                lat: 42.352271,
+                lng: -71.05524200000001
+            },
+            zoom: 14
+        });
+
+        var cars = [{
+            id : 'mXfkjrFw',
+            coords : {
+                lat : 42.3453,
+                lng: -71.0464
+
+            }}, {
+            id : 'nZXB8ZHz',
+            coords : {
+                lat : 42.3662,
+                lng: -71.0621
+            }}, {
+            id : 'Tkwu74WC',
+            coords : {
+                lat : 42.3603	,
+                lng: -71.0547
+            }}, {
+            id : '5KWpnAJN',
+            coords : {
+                lat : 42.3472,
+                lng: -71.0802
+            }}, {
+            id : 'uf5ZrXYw',
+            coords : {
+                lat : 42.3663,
+                lng: -71.0544
+            }}, {
+            id : 'VMerzMH8',
+            coords : {
+                lat : 42.3542,
+                lng: -71.0704
+            }
+        }];
+        for (var i = 0; i < cars.length; i++) {
+            console.log(cars[i].coords);
+            var marker = new google.maps.Marker({position: cars[i].coords, map: map});
+        }
+
+
+    }
+
+    exports.initMap = initMap;
+})((this.window = this.window || {}));
